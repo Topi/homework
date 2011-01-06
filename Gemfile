43438dbd5b007a5571e6ff703a3c863c98a33766
Topi <topi@noob.hu>
2011-01-06 10:45:46 +0100

    lesson 3 final commit

--- Gemfile
+++ Gemfile
@@ -31,9 +31,11 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :development do
   gem 'rspec-rails', '2.3.0'
+  gem 'spork', '0.8.4'
 end
 
 group :test do
   gem 'rspec', '2.3.0'
+  gem 'spork', '0.8.4'
   gem 'webrat', '0.7.1'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,4 +16,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
